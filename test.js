3953108fab7e1b0d7d2bf446067802a7dd4c87a1
Rakshihta <rakshithad410@gmail.com>
2026-05-28 16:30:10 +0530

    added branch

--- test.js
+++ test.js
@@ -2,10 +2,10 @@ const add = require('./add');
 
 if(add(2,3) === 5){
     console.log(" test successfull");
-    exit(0);
+    process.exit(0);
 }
 else{
     console.log("test failed");
-    exit(1);
+    process.exit(1);
 }
 
